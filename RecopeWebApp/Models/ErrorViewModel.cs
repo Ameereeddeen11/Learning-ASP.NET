e005287757db4a89fd0ace7a2060c841d89571d0
Amir Abdullaev <amir.abdullaev@emai.cz>
2023-09-28 12:09:44 +0200

    Learning ASP.NET from again

--- RecopeWebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RecopeWebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
