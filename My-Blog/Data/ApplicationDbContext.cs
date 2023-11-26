efb4aeb00b5545184abe22a34459315a376fe062
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2023-11-26 23:20:07 +0100

    start making web

---
+++ My-Blog/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using My_Blog.Models;
+
+namespace My_Blog.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<My_Blog.Models.Blog> Blog { get; set; } = default!;
+    }
+}
---
+++ My-Blog/Models/Blog.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace My_Blog.Models
+{
+    public class Blog
+    {
+        public Guid Id { get; set; }
+        public required string Title { get; set; }
+        public required string Content { get; set; }
+        public DateTime PublishedDate { get; set; }
+        [ForeignKey("UserId")]
+        //public UserId User { get; set; }
+    }
+}
